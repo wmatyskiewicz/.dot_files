91d831cbe023c3e95262bd86d7bb4b55c0227455
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-11-07 14:36:14 +0100

    django local settings

---
+++ ralph/settings-local.py
@@ -0,0 +1,36 @@
+DEBUG = True
+# False =  DjDT is close
+DEBUG_TOOLBAR = TRUE
+TEMPLATE_DEBUG = True
+
+SECRET_KEY = 'dev_key'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ralph',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        },
+    }
+
+if DEBUG_TOOLBAR is True:
+    INTERNAL_IPS = ('127.0.0.1',)
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+        )
