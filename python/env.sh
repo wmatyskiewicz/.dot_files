c3792e30d196894d69acf00270278ce14cda435f
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-01-18 17:25:38 +0100

    Init

---
+++ python/env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Configuration
+package[0]="bpython" # bpython is a fancy interface to the Python interpreter for Unix-like operating systems.
+package[1]="pdbpp" # pdb++, a drop-in replacement for pdb
+package[2]="pep8" # pep8 is a tool to check your Python code against some of the style conventions in PEP 8.
+
+
+
+
+# Create a virtual environment
+echo -e "\033[32m --------------------------------------------------------------------------------\e[00m"
+echo -e "\033[32m Creating a virtual environment\e[00m"
+echo -e "\033[32m --------------------------------------------------------------------------------\e[00m"
+
+virtualenv . --distribute --no-site-packages
+source bin/activate
+mkdir project
+
+# Instaling packages
+for ((i=0; i<${#package[*]}; i++)) 
+do
+  echo -e "\033[32m -------------------------------------------------------------------------------\e[00m"
+  echo -e "\033[32m Instaling ${package[$i]}...\e[00m"
+  echo -e "\033[32m -------------------------------------------------------------------------------\e[00m"
+  pip install ${package[$1]}
+  echo ""
+done
+
+deactivate
