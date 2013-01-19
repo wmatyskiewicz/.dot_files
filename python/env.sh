f35cea0aff468af7702b26b7c85508469a5c5e43
Wojciech Matyśkiewicz <wojtek@matyskiewicz.com>
2013-01-19 18:46:46 +0100

    newlines remove

--- python/env.sh
+++ python/env.sh
@@ -8,9 +8,6 @@ package[0]="bpython" # bpython is a fancy interface to the Python interpreter fo
 package[1]="pdbpp" # pdb++, a drop-in replacement for pdb
 package[2]="pep8" # pep8 is a tool to check your Python code against some of the style conventions in PEP 8.
 
-
-
-
 # Create a virtual environment
 echo -e "\033[32m --------------------------------------------------------------------------------\e[00m"
 echo -e "\033[32m Creating a virtual environment\e[00m"
