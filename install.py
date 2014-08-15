ec96f6e9d65969d85c4ba0d68d4058e50618c968
Wojtek Matyskiewicz <wojtek@matyskiewicz.com>
2014-08-15 14:39:09 +0200

    git config

--- install.py
+++ install.py
@@ -30,6 +30,7 @@ def install_ohmyzsh():
     _task_header('Install oh-my-zsh')
     call('curl -L http://install.ohmyz.sh | sh', shell=True)
 
+
 def config_workspace():
     _task_header('Configure Workspace')
     call('rm ../.zshrc', shell=True)
@@ -37,17 +38,17 @@ def config_workspace():
     call('mkdir Workspace', shell=True)
     call('sudo easy_install pip', shell=True)
 
+
 def configure_sublime():
     _task_header('Configure Sublime')
     call('rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User', shell=True)
     call('ln -s ~/.dot_files/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User', shell=True)
 
+
 def configure_git():
     _task_header('Configure git')   
-    call('git config --global user.name "Wojtek Matyskiewicz"', shell=True)
-    call('git config --global user.email "wojtek@matyskiewicz.com"', shell=True)
-    call('git config --global core.editor "subl -n -w"', shell=True)
-    call('git config --global core.excludesfile "~/.dot_files/git/.gitignore_global"', shell=True)
+    call('ln -s ~/.dot_files/git/.gitconfig ~/.gitconfig', shell=True)
+
 
 def install_python_packages():
     _task_header('Install python packages')
